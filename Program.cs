7321eb62fa998effe236a0eecf436a4d547818fa
Minsetr0 <nesterovmaksim71@mail.ru>
2023-01-29 23:29:26 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿bool isOpen = true;
+
+while (isOpen)
+{
+    Console.WriteLine("для выхода из цикла напишите exit");
+    string message = Console.ReadLine();
+    if (message == "exit")
+        break;
+    Console.Clear();
+}
